15ebde7a7a65fc4a39a4f75f751a8bdb78631649
Unni Ramachandran <unnipbvr@gmail.com>
2020-04-26 01:04:13 -0400

    Setting up AWS connection

--- server.js
+++ server.js
@@ -3,27 +3,26 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
+console.log("Env Variable", process.env.NODE_ENV);
 // // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Add routes, both API and view
-// const routes = require("./routes");
-// app.use(routes);
-
+const routes = require("./routes");
+app.use(routes);
 
 // var syncOptions = {};
 // syncOptions.force = process.env.SYNC_MODEL === "true" ? true : false;
-// var db = require("./models");
+var db = require("./models");
 
 // Starting the server, syncing our models ------------------------------------/
-// db.sequelizeConnection.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelizeConnection.sync().then(function () {
+  app.listen(PORT, function () {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      "Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
   });
-// });
+});
